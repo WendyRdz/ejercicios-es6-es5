b0c4436e44529ad1784ab7cf5b62407e19bb296c
WendyRdz <46804367+WendyRdz@users.noreply.github.com>
2019-01-23 12:39:24 -0600

    Update app.js

--- 6.-forOf/js/app.js
+++ 6.-forOf/js/app.js
@@ -1,12 +1,24 @@
                             // ES5
 
-// declara la variable
-
-
+/*var array = [10, 20, 30, 20, 30, 40, 60];
+var suma = 0;
 // escribe tu funcion
 
+for (var i = 0; i < array.length; i++) {
+  suma += array[i];
+}
+console.log("el promedio del array es:" + suma);*/
 
+//eS6
 
+const array = [10, 20, 30, 20, 30, 40, 60];
+let suma = 0;
+// escribe tu funcion
+
+for (let i = 0; i < array.length; i++) {
+    suma += array[i];
+}
+console.log(` el promedio del array es:${suma}`);
 
 
 
