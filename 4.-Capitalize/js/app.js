3ab57c8b974c846616083a164925b40fd829ce7f
WendyRdz <46804367+WendyRdz@users.noreply.github.com>
2019-01-22 23:28:48 -0600

    Update app.js

--- 4.-Capitalize/js/app.js
+++ 4.-Capitalize/js/app.js
@@ -1,6 +1,6 @@
 // En este ejercicio te daremos el código en ES6 ya solucionado, tu tendrás que transformarlo en ES5
 
-const word = "este ejercicio pasa a mayusculas la primer letra de cada palabra";
+/*const word = "este ejercicio pasa a mayusculas la primer letra de cada palabra";
 
 const capitalize = str => {
   let newStr = str.split(" ");
@@ -13,7 +13,28 @@ const capitalize = str => {
     console.log(result.join(" "));
    };	
 
-capitalize(word);
+capitalize(word);*/
 
 
 // Comenta el código anterior y ahora traducelo a ES5
+
+
+var word = "este ejercicio pasa a mayusculas la primer letra de cada palabra";
+
+var capitalize = function capitalize(str) {
+  var newStr = str.split(" ");
+
+  var result = [];
+
+  newStr.forEach(function(palabra) {
+    var primerLetra = palabra.charAt(0).toUpperCase();
+
+    var mediaPalabra = palabra.slice(1, palabra.length);
+
+    result.push(primerLetra + mediaPalabra);
+  });
+
+  console.log(result.join(" "));
+};
+
+capitalize(word);
